07bed0f7c094ffe6722eeebfa09637871b622212
clixff <10634323+clixff@users.noreply.github.com>
2021-04-04 20:27:21 +0300

    Initial commit

---
+++ Source/Chatters/ChattersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChattersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHATTERS_API AChattersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
